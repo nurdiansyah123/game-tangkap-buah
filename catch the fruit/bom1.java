952479b8b80dac72b84a75df1b55f488b9fc13c3
nurdiansyah123 <stsetia.nurdin@gmail.com>
2018-05-22 09:05:36 +0800

    push ke 6

--- catch the fruit/bom1.java
+++ catch the fruit/bom1.java
@@ -12,14 +12,14 @@ public class bom1 extends benda
      * Act - do whatever the bom1 wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
      */
-    private int timer=0;
+    private int timer=0;    
     public void act() 
     {
-        setLocation(getX(), getY()+8); 
-        LemonFall(); 
+        setLocation(getX(), getY()+9); 
+        bom1Fall();
     } 
     
-    public void  LemonFall() 
+    public void  bom1Fall() 
      {
          if (atWorldEdge()) 
          {
